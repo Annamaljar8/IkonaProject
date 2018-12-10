dec4b237e525e5fc56e3c0b3a47f35b8c1379f2f
Annamaljar8 <annamaljar8@gmail.com>
2018-12-10 13:41:33 +0100

    order form

--- File_JS/form_submit.js
+++ File_JS/form_submit.js
@@ -1,20 +1,6 @@
 eventListeners();
 function eventListeners(){
   const ui=new UI;
-  //display modal
-  const links=document.querySelectorAll('.work-item__icon');
-
-  links.forEach(function(item){
-    item.addEventListener('click', function(event){
-      ui.showModal(event);
-    })
-  })
-
-  //hide modal
-document.querySelector(".work-modal__close").addEventListener('click', function(){
-ui.closeModal();
- })
-}
 
 //submit the form
 document.querySelector(".order-form").addEventListener('submit',function(event){
@@ -33,30 +19,14 @@ document.querySelector(".order-form").addEventListener('submit',function(event){
     ui.showFeedback('Что-то заполнено неверно', 'error');
   }
 })
-
+}
 
 
 //constructor function
 function UI(){
 
 }
-//show modal
-UI.prototype.showModal=function (event) {
 
-  event.preventDefault();
-  if(event.target.parentElement.classList.contains('.work-item__icon'));
-  let id=event.target.parentElement.dataset.id;
-
-  const modal=document.querySelector('.work-modal');
-  const modalItem=document.querySelector('.work-modal__item');
-  modal.classList.add('work-modal__show');
-  //modal.classList.remove('work-modal');
-  modalItem.style.backgroundImage = `url(../Icona_Image/icona_work_${id}.jpg)`
-}
-//hide modal
-UI.prototype.closeModal = function(){
-  document.querySelector('.work-modal').classList.remove('work-modal__show');
-}
 
 //check for empty value
 
